01b44c5b368511fb95bd2c8a4c17006ad6a12d70
louiskonig <louiskonig@gmail.com>
2014-12-06 18:19:57 -0500

    Web services example
    Web services example

--- Web Services Example 2/ViewController.swift
+++ Web Services Example 2/ViewController.swift
@@ -8,13 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, LTMorphingLabelDelegate {
 
+
+    @IBOutlet weak var myNameLabel: LTMorphingLabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let manager = AFHTTPRequestOperationManager()
+        
+        manager.GET( "http:/graph.facebook.com/nancynyc",
+            parameters: nil,
+            success: { (operation: AFHTTPRequestOperation!,responseObject: AnyObject!) in
+                println("JSON: " + responseObject.description)
+            
+                if let myName = responseObject.valueForKey("name") as? String {
+                    
+                    self.myNameLabel.text = myName
+                    self.myNameLabel.morphingEffect = .Sparkle
+                }
+            
+            },
+            failure: { (operation: AFHTTPRequestOperation!,error: NSError!) in
+                println("Error: " + error.localizedDescription)
+        })
+        
         // Do any additional setup after loading the view, typically from a nib.
-    }
+    
+        
 
+    
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
---
+++ bridge.h
@@ -0,0 +1,13 @@
+//
+//  bridge.h
+//  Web Services Example 2
+//
+//  Created by Louis Konig on 12/4/14.
+//  Copyright (c) 2014 Louis Konig. All rights reserved.
+//
+
+#ifndef Web_Services_Example_2_bridge_h
+#define Web_Services_Example_2_bridge_h
+#import "AFNetworking/AFNetworking.h"
+
+#endif
